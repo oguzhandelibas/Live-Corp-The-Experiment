ef94fe252a4d126d217c8f64f38c31dddb1f8845
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-06-15 21:33:04 +0300

    +refactor: namespace

--- Assets/3_Scripts/MiniGames/YesYes/YesYesController.cs
+++ Assets/3_Scripts/MiniGames/YesYes/YesYesController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Player;
 using InfimaGames.LowPolyShooterPack;
 
 namespace MiniGame.YesYes
