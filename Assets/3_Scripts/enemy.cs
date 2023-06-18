19e56449a9290dd3ddd930d74f85dc47ae4cc371
Yağız Efe Atasever <ataseverefeyagiz@gmail.com>
2023-06-18 15:56:22 +0300

    NPC Update 2
    #1) oyuncu NPC'nin görüş alanına girdiğinde, NPC oyuncuyu kovalamaya başlıyor.
    
    #2) Oyuncu NPC'nin ateş menzilinde olduğunda NPC ateş etmeye başlıyor.
    
    #3) NPC, istenen iki nokta arasında git gel yaparak devriye gezebilir. Bu noktalar belirlenmemişse, NPC rastgele dolanır.

--- Assets/3_Scripts/enemy.cs
+++ Assets/3_Scripts/enemy.cs
@@ -1,125 +1,118 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.UIElements;
+ï»¿
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace NPC
+public class enemy : MonoBehaviour
 {
-    public class enemy : MonoBehaviour
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
-        public NavMeshAgent agent;
-        public Transform player;
-        public LayerMask whatIsGround, whatIsPlayer;
-        public GameObject projectile;
-        public float health;
+        player = GameObject.Find("MainCamera").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-        //Patrolling - Devriye variables
-        public Vector3 walkPoint;
-        bool walkPointSet;
-        public float walkPointRange;
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        //Attacking - Saldýrý variables
-        public float timeBetweenAttacks;
-        bool alreadyAttacked;
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
 
-        //States
-        public float sightRange, attackRange;
-        public bool playerInSightRange, playerInAttackRange;
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
 
-        private void Awake()
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
         {
-            player = GameObject.Find("Player").transform;
-            agent = GetComponent<NavMeshAgent>();
+            ///Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 20f, ForceMode.Impulse);
+            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 
-        private void Update()
-        {
-            //Check for sight and attack range - görüþ alaný ve saldýrý menzili algýlama
-            playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
 
-            if (!playerInAttackRange && !playerInSightRange)
-                Patroling();
-            if (!playerInAttackRange && playerInSightRange)
-                ChasePlayer();
-            if (playerInAttackRange && playerInSightRange)
-                AttackPlayer();
-        }
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
 
-        private void Patroling()
-        {
-            if (!walkPointSet)
-                SearchWalkPoint();
-
-            if(walkPointSet)
-                agent.SetDestination(walkPoint);
-
-            Vector3 distanceToWalkpoint = transform.position - walkPoint;
-
-            //Walkpoint reached - ulaþýldý
-            if(distanceToWalkpoint.magnitude < 1f)
-                walkPointSet = false;
-        }
-
-        private void SearchWalkPoint()
-        {
-            //Calculate random point in range - menzilde rastgele bir nokta hesapla
-            float randomZ = Random.Range(-walkPointRange, walkPointRange);
-            float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-            if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-                walkPointSet = true;
-        }
-
-        private void ChasePlayer()
-        {
-            agent.SetDestination(player.position);
-        }
-
-        private void AttackPlayer()
-        {
-            //make sure agent dont move - hareket etmediðinden emin ol
-            agent.SetDestination(transform.position);
-            transform.LookAt(player);
-
-            if (!alreadyAttacked)
-            {
-                //Attack code here
-                Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-                rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-
-                alreadyAttacked = true;
-                Invoke(nameof(ResetAttack), timeBetweenAttacks);
-            }
-        }
-
-        private void ResetAttack()
-        {
-            alreadyAttacked = false;
-        }
-
-        public void TakeDamage(int Damage)
-        {
-            health -= Damage;
-            if (health <= 0)
-                Invoke("DestroyEnemy", 0.5f);
-        }
-
-        private void DestroyEnemy()
-        {
-            Destroy(gameObject);
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, attackRange);
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, sightRange);
-        }
-
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
