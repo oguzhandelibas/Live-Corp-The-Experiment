3788d4d4f4b41026ddbd05884fee6ebf03bf2356
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-06-28 17:38:23 +0300

    Revert "Merge branch 'efe' into Oğuzhan"
    This reverts commit 308293c69ad58d968580e1cab27204457d7baef9, reversing
    changes made to 3763cb7010f574ca37025d9e036309e057dd1acc.

--- Assets/9_ThirdParty/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Legacy/Projectile.cs
+++ Assets/9_ThirdParty/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Legacy/Projectile.cs
@@ -6,8 +6,6 @@ using MiniGame.DoorGame;
 using Random = UnityEngine.Random;
 
 using MiniGame.RaidGame;
-using UnityEditor;
-
 public class Projectile : MonoBehaviour {
 
 	[Range(2, 100)]
@@ -150,22 +148,7 @@ public class Projectile : MonoBehaviour {
 		{
 			woodBreak.Break();
 		}
-
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            //Debug.Log("vurdun");
-            BodyPart bodyPart = collision.gameObject.GetComponent<BodyPart>();
-
-            if (bodyPart != null && bodyPart.npcHealth != null)
-            {
-                // Call TakeDamage on the NPCHealth
-                bodyPart.npcHealth.TakeDamage(50);
-            }
-
-            //Destroy bullet object
-            Destroy(gameObject);
-        }
-    }
+	}
 
 	private IEnumerator DestroyTimer () 
 	{
